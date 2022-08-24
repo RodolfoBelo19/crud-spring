99e9f9f7f0eacdcd261c048abe31618e3de0aae6
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2022-08-24 17:16:35 -0300

    fix

--- src/main/java/com/rodolfo/crudspring/CrudSpringApplication.java
+++ src/main/java/com/rodolfo/crudspring/CrudSpringApplication.java
@@ -1,7 +1,11 @@
 package com.rodolfo.crudspring;
 
+import com.rodolfo.crudspring.model.Course;
+import com.rodolfo.crudspring.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,6 +14,19 @@ public class CrudSpringApplication {
 
 		SpringApplication.run(CrudSpringApplication.class, args);
 
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository) {
+		return args -> {
+				courseRepository.deleteAll();
+
+				Course c = new Course();
+				c.setName("Angular com Spring");
+				c.setCategory("front-end");
+
+				courseRepository.save(c);
+		};
 	}
 
 }
---
+++ src/main/java/com/rodolfo/crudspring/model/Course.java
@@ -0,0 +1,23 @@
+package com.rodolfo.crudspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private  String category;
+
+}
